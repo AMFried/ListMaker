1696624f0bcd21fb9597157a5b794927bbe79eaf
PunisherLex <alex.friedlander@gmail.com>
2020-09-05 22:08:25 -0400

    Chapter 8 commit
    ListDataManager.kt & TaskList.kt created
    
    ListDataManager.kt does what it says on the tin
    While
    TaskList.kt works as an Object type
    
    4 files modified
    - ListSelectionRecyclerViewAdapter.kt
    - MainActivity.kt
    - activity_main.xml
    - strings.xml
    
    Of note: Removed more vestigial nav related code/resources from strings.xml
     and code instruction in the book uses deprecated code (at least in java) causing unnecessary warnings.

--- app/src/main/java/com/raywenderlich/listmaker/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/com/raywenderlich/listmaker/ListSelectionRecyclerViewAdapter.kt
@@ -4,9 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter<ListSelectionViewHolder>() {
-
-    val listTitles= arrayOf("Shopping List","Chores","Android Tutorials")
+class ListSelectionRecyclerViewAdapter(private val lists : ArrayList<TaskList>) : RecyclerView.Adapter<ListSelectionViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_selection_view_holder,parent, false)
@@ -15,11 +13,17 @@ class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter<ListSelectionViewH
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
         holder.listPosition.text = (position + 1).toString()
-        holder.listTitle.text = listTitles[position]
+        holder.listTitle.text = lists.get(position).name
+
     }
 
     override fun getItemCount(): Int {
-        return listTitles.size
+        return lists.size
+    }
+
+    fun addList(list: TaskList) {
+        lists.add(list)
+        notifyItemInserted(lists.size-1)
     }
 
 }
